25fc32ac17b26e7ca1accf4d34a3b344183e31a4
Tori Rodgers <torirodgers22@gmail.com>
2024-08-27 21:26:25 -0500

    code for Banner componenet, imported to App.jsx

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './styling.css';
+
+const Banner = () => {
+        return (
+         <div>
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see the satellites in that orbit type.</p>
+         </div>
+        );
+}
+
+
+export default Banner;
