0c005ed5c31f3265c241ed9b4d0bdd857deedf60
Tori Rodgers <torirodgers22@gmail.com>
2024-08-27 22:12:27 -0500

    finished task 5

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,19 +1,28 @@
 import classes from './styling.css';
 
 const Table = ({ sat }) => {
+  let buttonFilter = sat.map((sat, id) =>
+    <tr key={id}>
+      <td>{sat.name}</td>
+      <td>{sat.type}</td>
+      <td>{sat.launchDate}</td>
+      <td>{sat.operational === true ? "Active" : "Inactive"}</td>
+    </tr>
+  );
 
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {buttonFilter}
+      </tbody>
    </table>
   );
 };
